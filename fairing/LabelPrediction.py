68876fba06e1dc9c5973f088dd50066ab847c641
rahulku <rahulskumar@gmail.com>
2019-11-05 13:13:14 -0800

    Update LabelPrediction.py

--- fairing/LabelPrediction.py
+++ fairing/LabelPrediction.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# TEST IGNORE PLEASE
+
 """Use Append builder and deploy the model.
 
 The file needs to be named the same as the class containing the predict method.
